670810efe7c2ddab949077c897b68238a3dbd812
james gonzales molina <jamesgonzalesmolina99@gmail.com>
2023-06-05 08:06:40 -0300

    2-PRE ENTREGA + JAMES GONZALES MOLINA

--- js/main.js
+++ js/main.js
@@ -1,31 +1,66 @@
-function calcularEdad() {
-    let nombre = prompt("ingrese su nombre");
-    let apellido = prompt("ingrese su apellido");
-    let edad = prompt("ingresa tu edad");
-    let categoria ;
-
-    if (edad >= 1 && edad <= 10 ) {
-         categoria = "niño";
-    }else if (edad >= 10 && edad <= 13){
-         categoria = "pre-adolecente";
-    }else if (edad >= 14 && edad <= 18) {
-         categoria = "adolecente";
-    }else if (edad >= 19 && edad <= 25) {
-         categoria = "joven";
-    }else if (edad >= 26 && edad <= 59) {
-         categoria = "adulto";
-    }else if (edad >= 60 && edad <= 75){
-         categoria = "viejo";
-   }else if (edad >= 76 && edad <= 110) {
-         categoria = "muy viejo";
-   }else {
-        categoria = "edad no valida";
-   }           
-
-    while (edad > 111) {
-         alert("ni que fueras dinosaurio");
-         edad =prompt("ingresa una edad menor");
-    }
-    alert("hola " + nombre + " " + apellido + "\ntu edad es de: " + edad + " " + "años \n" + "tu categoria de vida es de: " + categoria);
-}
-calcularEdad();
+const figurasPokemon = [
+     { nombre: "Pikachu", tipo: "Eléctrico" },
+     { nombre: "Charizard", tipo: "Fuego" },
+     { nombre: "Bulbasaur", tipo: "Planta" },
+     { nombre: "Squirtle", tipo: "Agua" },
+     { nombre: "Jigglypuff", tipo: "Hada" }
+   ];
+   
+   let figurasSeleccionadas = [];
+   
+   function seleccionarPokemon() {
+     const figura = prompt("selecciona una figura de Pokemon:\n" +
+       "1. Pikachu\n" +
+       "2. Charizard\n" +
+       "3. Bulbasaur\n" +
+       "4. Squirtle\n" +
+       "5. Jigglypuff\n\n" +
+       "ingresa el numero correspondiente:");
+   
+     return figurasPokemon[figura - 1];
+   }
+   
+   function ingresarCantidad() {
+     const cantidad = prompt("ingresa la cantidad de figuras de Pokemon que quieras tener:");
+     return +cantidad;
+   }
+   
+   function comprarOtroPokemon() {
+     const opcion = prompt("¿Desea comprar otro Pokemon? (Si/No)").toLowerCase();
+     return opcion === "si";
+   }
+   
+   do {
+     const pokemon = seleccionarPokemon();
+     const cantidad = ingresarCantidad();
+   
+     figurasSeleccionadas.push({ pokemon, cantidad });
+   } while (comprarOtroPokemon());
+   
+   let mensaje = "has elejido :\n\n";
+   for (const figuraSeleccionada of figurasSeleccionadas) {
+     const figura = figuraSeleccionada.pokemon.nombre;
+     const cantidad = figuraSeleccionada.cantidad;
+     mensaje += "Pokemon: " + figura + " - Cantidad: " + cantidad + "\n";
+   }
+   
+   alert(mensaje);
+   
+   const tipoBuscado = "Fuego";
+   const pokemonTipoFuego = figurasPokemon.filter(function(pokemon) {
+     return pokemon.tipo === tipoBuscado;
+   });
+   let mensajeTipoFuego = "Pokemons de tipo fuego:\n\n";
+   for (const pokemon of pokemonTipoFuego) {
+     mensajeTipoFuego += "Pokemon: " + pokemon.nombre + " - Tipo: " + pokemon.tipo + "\n";
+   }
+   alert(mensajeTipoFuego);
+   
+   const nombreBuscado = "Pikachu";
+   const pokemonEncontrado = figurasPokemon.find(function(pokemon) {
+     return pokemon.nombre === nombreBuscado;
+   });
+   let mensajePokemonEncontrado = "Pokemon encontrado:\n\n";
+   mensajePokemonEncontrado += "Pokemon: " + pokemonEncontrado.nombre + " - Tipo: " + pokemonEncontrado.tipo;
+   alert(mensajePokemonEncontrado);
+   
